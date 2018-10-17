612886cd362bff5d136df3d35d7e5ef8cede6ae3
Samir <samnik_12345@hotmail.com>
2018-10-17 13:19:22 -0400

    updated requirements.txt 10/17 @1:19

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framework.authtoken', #Checked this is good
     'profiles_api',
 ]
 
