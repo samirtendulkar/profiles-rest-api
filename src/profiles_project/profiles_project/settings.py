79097cc8b7c6e102db29fa67e7d4d9c9577b4212
Samir Tendulkar <38055917+samirtendulkar@users.noreply.github.com>
2018-10-21 05:58:39 -0400

    Update settings.py

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$75f=4pf4g1y4v9ihu5dsr7(!v=bkoa%tp@ia=an02u&+z#)2d'
+SECRET_KEY = '$75f=4pf4gJGJHGJGKGKJHUiuyyuguyr#$@$#@$#@%@^7fhjfp@ia=an02u&+z#)2d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
